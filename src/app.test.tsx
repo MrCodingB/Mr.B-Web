75eaf8675fb0ef93c92d9102aadc400a37bfb39a
MrCodingB <bastiwoess@gmail.com>
2021-08-22 01:02:39 +0200

    Fix expecting array to be in document

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -4,8 +4,8 @@ import App from './app';
 
 test('renders home page links', () => {
   render(<App />);
-  expect(screen.getAllByText(/about me/i)).toBeInTheDocument();
-  expect(screen.getAllByText(/coding/i)).toBeInTheDocument();
-  expect(screen.getAllByText(/other/i)).toBeInTheDocument();
-  expect(screen.getAllByText(/contact/i)).toBeInTheDocument();
+  expect(screen.getAllByText(/about me/i)[0]).toBeInTheDocument();
+  expect(screen.getAllByText(/coding/i)[0]).toBeInTheDocument();
+  expect(screen.getAllByText(/other/i)[0]).toBeInTheDocument();
+  expect(screen.getAllByText(/contact/i)[0]).toBeInTheDocument();
 });
