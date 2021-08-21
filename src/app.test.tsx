053252f74c9ad40a1c397a6cc62582970e4eb028
MrCodingB <bastiwoess@gmail.com>
2021-08-22 00:58:56 +0200

    Fix link names

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -4,8 +4,8 @@ import App from './app';
 
 test('renders home page links', () => {
   render(<App />);
-  expect(screen.getByText(/about/i)).toBeInTheDocument();
-  expect(screen.getByText(/code/i)).toBeInTheDocument();
+  expect(screen.getByText(/about me/i)).toBeInTheDocument();
+  expect(screen.getByText(/coding/i)).toBeInTheDocument();
   expect(screen.getByText(/other/i)).toBeInTheDocument();
   expect(screen.getByText(/contact/i)).toBeInTheDocument();
 });
