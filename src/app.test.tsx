d77eb16bf1e92a5ed08ce92ce97d875ad33c4ef9
MrCodingB <bastiwoess@gmail.com>
2021-08-22 00:55:43 +0200

    Fix tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './app';
 
-test('renders learn react link', () => {
+test('renders home page links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(screen.getByText(/about/i)).toBeInTheDocument();
+  expect(screen.getByText(/code/i)).toBeInTheDocument();
+  expect(screen.getByText(/other/i)).toBeInTheDocument();
+  expect(screen.getByText(/contact/i)).toBeInTheDocument();
 });
