3c785cfdc3f96f4c56b52c706c56e217d02e340e
MrCodingB <bastiwoess@gmail.com>
2021-08-22 01:01:04 +0200

    Allow multiple results in tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -4,8 +4,8 @@ import App from './app';
 
 test('renders home page links', () => {
   render(<App />);
-  expect(screen.getByText(/about me/i)).toBeInTheDocument();
-  expect(screen.getByText(/coding/i)).toBeInTheDocument();
-  expect(screen.getByText(/other/i)).toBeInTheDocument();
-  expect(screen.getByText(/contact/i)).toBeInTheDocument();
+  expect(screen.getAllByText(/about me/i)).toBeInTheDocument();
+  expect(screen.getAllByText(/coding/i)).toBeInTheDocument();
+  expect(screen.getAllByText(/other/i)).toBeInTheDocument();
+  expect(screen.getAllByText(/contact/i)).toBeInTheDocument();
 });
