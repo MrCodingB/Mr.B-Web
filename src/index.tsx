e55013b5cfa63997ed8c3376ceac1c21bb703c7d
MrCodingB <bastiwoess@gmail.com>
2021-08-22 00:57:22 +0200

    Move browser router to app component

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
-import { BrowserRouter } from 'react-router-dom';
 
 import './styles/styles.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
